a87d811c677f6b3c2604bc6eaa96fb756b14beaa
bdpaul99 <bddpaul@me.com>
2017-03-24 13:15:35 -0400

    added starter fikes

---
+++ twitter_info.py
@@ -0,0 +1,4 @@
+consumer_key = "fW4TKEXe3wNu33vUyEx98wJEA" 
+consumer_secret = "7vAFVsFUpQaxBbEdXZTj9DcPUDsbI1pvNbZllXQWQmHPpH7o6g"
+access_token = "336251929-Hh1UNng5aGvr3V4OxtcD4lI1glwO4zCFiFalOlvB"
+access_token_secret = "ts4Ow90m1mElQdiig6KVAcNpXZ5wqygDIUVFOKuikamTi"
